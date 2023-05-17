db3ae2f01de0aee1141332ab48f12b1f220b7a5b
Evan Perry <evmaperry@gmail.com>
2023-05-17 00:19:03 +0000

    recursions

--- src/recursion.js
+++ src/recursion.js
@@ -129,15 +129,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
-  string = string.replaceAll(" ", "")
+  string = string.replaceAll(" ", "").toLowerCase();
   if (string.length === 1 || string.length === 0){
     return true;
   }
 
-  if (string[0] !== string[string.length-1]){
+  if (!(string[0] === string[string.length - 1])){
     return false;
   }
-  return palindrome(string.slice(1, string.length));
+  return palindrome(string.slice(1, string.length -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
